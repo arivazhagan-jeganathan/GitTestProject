8f1044878e63b62ff39835b0a6ddf046e0b4a2ab
arivazhagan <arivazhagan@mookambikainfo.com>
2015-11-04 19:57:04 +0530

    master reset

--- src/test/newly/pack/RemoteFile.java
+++ src/test/newly/pack/RemoteFile.java
@@ -4,6 +4,8 @@ public class RemoteFile{
   
     public static void main(String[] args){
       System.out.println("file freshly created from git hub site");
+      
+      System.out.println("file updated from eclipse in local repository");
     }
 
 
---
+++ src/test/revert/pack/HelloWorld.java
@@ -0,0 +1,14 @@
+package test.revert.pack;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("file to be reverted");
+		
+		System.out.println("transient comment");
+		/*
+		 * Comment
+		 */
+	}
+
+}
