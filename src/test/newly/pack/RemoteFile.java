1aa2211056084078bc79e63642243e62d49aed9b
arivazhagan-jeganathan <arivazhagan@mookambikainfo.com>
2015-11-04 17:55:26 +0530

    RemoteFile.java
    File created from remote repository

---
+++ src/test/newly/pack/RemoteFile.java
@@ -0,0 +1,10 @@
+package test.newly.pack;
+
+public class RemoteFile{
+  
+    public static void main(String[] args){
+      System.out.println("file freshly created from git hub site");
+    }
+
+
+}
