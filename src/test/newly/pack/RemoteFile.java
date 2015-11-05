d94936813ae9926f167be2ed6761b18ef77acbc3
arivazhagan <arivazhagan@mookambikainfo.com>
2015-11-05 10:19:21 +0530

    git revert RemoteFile

--- src/test/newly/pack/RemoteFile.java
+++ src/test/newly/pack/RemoteFile.java
@@ -6,6 +6,8 @@ public class RemoteFile{
       System.out.println("file freshly created from git hub site");
       
       System.out.println("file updated from eclipse in local repository");
+      
+      System.out.println("new changes in local repository");
     }
 
 
